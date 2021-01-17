38e6b8f4b996f6c104e93f4f2f3d09bc66b0def9
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-01-17 23:35:33 +0500

    Добавьте файлы проекта.

---
+++ Sotrudnik/Data/EmployeesContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Employees.Models;
+
+namespace Employees.Data
+{
+    public class EmployeesContext : DbContext
+    {
+        public EmployeesContext (DbContextOptions<EmployeesContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Employees.Models.Employee> Employee { get; set; }
+    }
+}
